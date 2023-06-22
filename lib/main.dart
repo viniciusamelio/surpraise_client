5efa506dc5996c0a00193bb370868aab6a4bbb35
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-06-22 12:33:43 -0300

    chore: setting up appwrite service on main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'core/dependencies.dart';
+import 'core/core.dart';
 
 import 'app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await setupDependencies();
+  await AppWriteService.init();
   runApp(const App());
 }
