42c6037e1e6944fd5fef406d851428853d3205bf
Vinicius Amélio <vinicius.amelio@cubos.io>
2023-10-18 22:06:13 -0300

    fix🐞: body structure & response type

--- lib/shared/presentation/organisms/new_praise_sheet.dart
+++ lib/shared/presentation/organisms/new_praise_sheet.dart
@@ -36,10 +36,11 @@ class _NewPraiseSheetState extends State<NewPraiseSheet> {
     controller.state.listenState(
       onSuccess: (right) {
         if (right.praiseData != null) {
-          injected<SupabaseCloudClient>()
-              .supabase
-              .functions
-              .invoke("notificator", body: right.praiseData);
+          injected<SupabaseCloudClient>().supabase.functions.invoke(
+                "notificator",
+                body: {"praise": right.praiseData!},
+                responseType: ResponseType.text,
+              );
         }
 
         const SuccessSnack(message: "Praise enviado!").show(context: context);
